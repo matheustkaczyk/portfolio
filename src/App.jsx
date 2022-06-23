06c0d64a5b8f52e1d0258cdc1761d1382deaaed6
matheustkaczyk <matheus111904@hotmail.com>
2022-06-23 11:09:41 -0300

    feat: instalando reacttypingeffect lib, renderizando landingpage, fazendo lib typer funcionar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.scss'
+
 import Sidebar from './components/Sidebar'
+
+import Landing from './pages/Landing'
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
+      <Landing />
     </div>
   )
 }
--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,9 +1,22 @@
+import React from 'react';
+import ReactTypingEffect from 'react-typing-effect';
+
 const Landing = () => {
   return(
-    <div>
+    <div className="landing">
       <h1>Olá,</h1>
       <h1>Me chamo Matheus Tkaczyk</h1>
       <h1>Desenvolvedor Web</h1>
+      <ReactTypingEffect
+        text={['Frontend', 'Backend', 'Fullstack']}
+        className={'typer'}
+        speed={50}
+        eraseSpeed={50}
+        eraseDelay={50}
+        typingDelay={50}
+      />
     </div>
   )
 }
+
+export default Landing;
