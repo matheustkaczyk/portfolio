434bda449674852e00fef05e8272e08a2795d990
matheustkaczyk <matheus111904@hotmail.com>
2022-07-06 11:30:27 -0300

    feat: renderizando logo svg no aside

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,10 +1,11 @@
 import { AiFillHome, AiFillInfoCircle, AiFillLinkedin, AiFillGithub, AiOutlineWhatsApp } from 'react-icons/ai';
 import { MdOutlineWork } from 'react-icons/md';
-import Logo from '../../images/web-development.svg';
+import Logo from '../../images/web-development.svg?component';
 
 const Sidebar = () => {
   return(
     <aside>
+      <Logo />
       <nav>
         <ul>
           <li><AiFillHome /></li>
