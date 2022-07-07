7567ac943d26cde638c602593acfabd7d05e3c21
matheustkaczyk <matheus111904@hotmail.com>
2022-07-07 11:16:14 -0300

    fix: alterando pequeno trecho do texto no about

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -10,7 +10,7 @@ const About = () => {
         <div>
           <p>
             Sou <span>apaixonado</span> por ciência e tecnologia, no momento estou em transição de carreira para o mundo
-            do desenvolvimento.<br/> Estudei no bootcamp de 1 ano da Trybe, onde aprendi um novo universo de linguagens e tecnologias
+            do desenvolvimento.<br/> Além de estudar por conta própria, fiz o bootcamp de 1 ano da Trybe, onde aprendi um novo universo de linguagens e tecnologias
             no mundo Fullstack.
           </p>
           <span>Hobbies:</span>
