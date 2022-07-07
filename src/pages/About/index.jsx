3c6a4b516b83bcc1c70b0f20e22fb439952611e1
matheustkaczyk <matheus111904@hotmail.com>
2022-07-07 11:14:43 -0300

    feat: estrutura e estilizacao da parte do about concluida

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,9 +1,26 @@
 import euImage from '../../images/tkaczyk.jpg';
 
 const About = () => {
+  const date = new Date();
+
   return(
     <section className="about">
-      <img src={euImage} alt="Foto Matheus Tkaczyk" />
+      <div>
+        <img src={euImage} alt="Foto Matheus Tkaczyk" />
+        <div>
+          <p>
+            Sou <span>apaixonado</span> por ciência e tecnologia, no momento estou em transição de carreira para o mundo
+            do desenvolvimento.<br/> Estudei no bootcamp de 1 ano da Trybe, onde aprendi um novo universo de linguagens e tecnologias
+            no mundo Fullstack.
+          </p>
+          <span>Hobbies:</span>
+          <ul>
+            <li>Gaming</li>
+            <li>Leitura</li>
+            <li>Séries</li>
+          </ul>
+        </div>
+      </div>
     </section>
   )
 }
