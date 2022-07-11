18227cd9ad67b609d227c12a3910e8b4de120f0e
matheustkaczyk <matheus111904@hotmail.com>
2022-07-11 09:25:38 -0300

    fix: removendo react-slick, não funcionou como eu gostaqria

--- src/pages/Tools/index.jsx
+++ src/pages/Tools/index.jsx
@@ -1,19 +1,10 @@
-import Slider from 'react-slick';
+import JavaScriptLogo from '../../images/javascript.svg?component';
+import ReactLogo from '../../images/react.svg?component';
 
 const Tools = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 200,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
-
   return(
     <section className="tools" id="tools">
-      <Slider settings={settings}>
 
-      </Slider>
     </section>
   )
 }
