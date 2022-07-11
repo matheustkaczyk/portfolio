2054d98f5bdd7f485e51f186cd859dd78574d831
matheustkaczyk <matheus111904@hotmail.com>
2022-07-11 09:40:21 -0300

    feat: efetuando todas as importacoes das imagens svg

--- src/pages/Tools/index.jsx
+++ src/pages/Tools/index.jsx
@@ -1,5 +1,16 @@
 import JavaScriptLogo from '../../images/javascript.svg?component';
+import HtmlLogo from '../../images/htmo.svg?component';
+import CssLogo from '../../images/css.svg?component';
 import ReactLogo from '../../images/react.svg?component';
+import ReduxLogo from '../../images/redux.svg?component';
+
+import TypescriptLogo from '../../images/typescript.svg?component';
+
+import NodeLogo from '../../images/nodejs.svg?component';
+import ExpressLogo from '../../images/express.svg?component';
+
+import MysqlLogo from '../../images/mysql.svg?component';
+import MongodbLogo from '../../images/mongodb.svg?component';
 
 const Tools = () => {
   return(
