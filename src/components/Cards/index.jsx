682d177cc990a9bd23680a5501b830a8dff3544d
matheustkaczyk <matheus111904@hotmail.com>
2022-07-12 09:00:04 -0300

    feat: criando componente de cards, estruturando componente

---
+++ src/components/Cards/index.jsx
@@ -0,0 +1,14 @@
+const Cards = () => {
+  return(
+    <article className="flip-card">
+      <div className="flip-card-inner">
+        <div className="flip-card-front">
+
+        </div>
+        <div className="flip-card-back">
+
+        </div>
+      </div>
+    </article>
+  )
+}
