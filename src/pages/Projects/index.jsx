203a2acfe56f42f1c7cf75c3d4fc3595efc1a4af
matheustkaczyk <matheus111904@hotmail.com>
2022-07-12 09:19:07 -0300

    feat: organizando informações sobre os projetos

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,4 +1,31 @@
 const Projects = () => {
+  const projectsInfo = [
+    {
+      name: 'Webchat',
+      text: 'Aplicação de webchat, feita utilizando React em seu front-end e um back-end em Node.js utilizando Socket.io.',
+      githubLink: 'https://github.com/matheustkaczyk/web-chat-react',
+      deploymentLink: 'https://webchattkaczyk.netlify.app/'
+    },
+    {
+      name: 'EasyBank',
+      text: 'Aplicação de uma Landing Page de um banco, feita utilizando React e SASS.',
+      githubLink: 'https://github.com/matheustkaczyk/easybank-challenge',
+      deploymentLink: 'https://easybank-one-omega.vercel.app/'
+    },
+    {
+      name: 'QRCode Generator',
+      text: 'Aplicação de um gerador de QR Code, feita utilizando React e SASS.',
+      githubLink: 'https://qrcode-gen-tka.netlify.app/',
+      deploymentLink: 'https://github.com/matheustkaczyk/qrcode-generator'
+    },
+    {
+      name: 'Splitter',
+      text: 'Aplicação que divide custos, podendo contemplar também a porcentagem de gorjeta, feita utilizando React e SASS.',
+      githubLink: 'https://github.com/matheustkaczyk/splitter-challenge',
+      deploymentLink: 'https://splitter-challenge.vercel.app/'
+    },
+  ]
+
   return(
     <section className="projects">
       <h2>Projetos</h2>
