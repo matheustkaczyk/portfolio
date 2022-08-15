d42d7abab251184b9de43db4c4104f4d7a723e65
matheustkaczyk <matheus111904@hotmail.com>
2022-08-15 10:59:13 -0300

    refact: corrigindo layout do header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,6 @@
 import { AiFillHome, AiFillInfoCircle, AiFillLinkedin, AiFillGithub, AiOutlineWhatsApp } from 'react-icons/ai';
 import { MdOutlineWork } from 'react-icons/md';
+import { BsTools } from 'react-icons/bs';
 import Logo from '../../images/web-development.svg?component';
 
 const Header = () => {
@@ -10,15 +11,15 @@ const Header = () => {
         <ul>
           <li><a href='#home'><AiFillHome /></a></li>
           <li><a href='#about'><AiFillInfoCircle /></a></li>
-          {/* <li><a href="#tools"><BsTools /></a></li> */}
+          <li><a href="#tools"><BsTools /></a></li>
           <li><a href="#projects"><MdOutlineWork /></a></li>
         </ul>
+        {/* <div className='social'>
+          <li><AiFillLinkedin className='linkedin' /></li>
+          <li><AiFillGithub className='github' /></li>
+          <li><AiOutlineWhatsApp className='whatsapp' /></li>
+        </div> */}
       </nav>
-      <div className='social'>
-        <li><AiFillLinkedin className='linkedin' /></li>
-        <li><AiFillGithub className='github' /></li>
-        <li><AiOutlineWhatsApp className='whatsapp' /></li>
-      </div>
     </header>
   )
 }
